559f1ca6421e5e1c6f42ef04e0f625eee3560227
sasha-pylypchak <pylypchak.oleksandr.71@gmail.com>
2018-12-13 18:05:42 +0200

    add social icon

--- header.php
+++ header.php
@@ -9,52 +9,22 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
+    <header class="top_header">
+        <div class="wrapper cf">
+            <div class="header_third search_section menu_buttom">
+                search
+            </div>
+            <div class="header_third logo_section">
+                <a href="<?php echo home_url("/");?>" class="logo_link"><img src="<?php echo ale_get_option('sitelogo'); ?>" /></a>
+            </div>
+            <div class="header_third social_section">
+                <?php if(aletheme_get_options('yt')){ ?><a href="<?php echo aletheme_get_options('yt') ?>"><i class="fa fa-youtube" aria-hidden="true"></i></a><?php } ?>
+                <?php if(aletheme_get_options('insta')){ ?><a href="<?php echo aletheme_get_options('insta') ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a><?php } ?>
+                <?php if(aletheme_get_options('twi')){ ?><a href="<?php echo aletheme_get_options('twi') ?>"><i class="fa fa-twitter-square" aria-hidden="true"></i></a><?php } ?>
+                <?php if(aletheme_get_options('fb')){ ?><a href="<?php echo aletheme_get_options('fb') ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a><?php } ?>
+            </div>
+        </div>
+    </header>
+    <section class="home_slider">
 
-<?php /*if(ale_get_option('sitelogo')){
-    echo "<img src='".ale_get_option('sitelogo')."' />";
-}*/ ?>
-<?php ale_option('sitelogo'); ?>
-
-<section class="slider-example">
-	<div class="newhomeslider wrapper">
-		<ul class="slides">
-			<?php $slider = ale_sliders_get_slider('test-slider');  ?>
-			<?php if($slider):?>
-				<?php foreach ($slider['slides'] as $slide) : ?>
-					<li>
-						<figure>
-							<img src="<?php echo $slide['image']; ?>" alt="<?php echo $slide['title']; ?>" />
-							<figcaption>
-								<div class="sliderdata">
-									<?php if($slide['title']){ ?>
-										<div class="titleslide headerfont">
-											<?php if($slide['url']){
-												echo "<a href='".$slide['url']."'>";
-											} ?>
-
-											<?php echo $slide['title']; ?>
-
-											<?php if($slide['url']){
-												echo "</a>";
-											} ?>
-										</div>
-									<?php } ?>
-									<?php if($slide['description']){ ?>
-										<div class="descriptionslide">
-											<?php echo $slide['description']; ?>
-										</div>
-									<?php } ?>
-									<?php if($slide['html']){ ?>
-										<div class="descriptionslide">
-											<?php echo $slide['html']; ?>
-										</div>
-									<?php } ?>
-								</div>
-							</figcaption>
-						</figure>
-					</li>
-				<?php endforeach; ?>
-			<?php endif;?>
-		</ul>
-	</div>
-</section>
+    </section>
